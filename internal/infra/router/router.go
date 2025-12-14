c3b077352df5998c9eaf3c152962eb8787a859fa
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-12-14 22:50:08 +0900

    refactor: route

--- internal/infra/router/router.go
+++ internal/infra/router/router.go
@@ -14,23 +14,24 @@ func RegisterRouter(e *echo.Echo, h *controller.Handler, auth *controller.AuthHa
 
 	protected := api.Group("")
 	protected.Use(authMiddleware)
+	{
+		protected.GET("/collections", h.ListCollections)
+		protected.GET("/collections/:id", h.GetCollection)
+		protected.POST("/collections", h.CreateCollection)
 
-	protected.GET("/collections", h.ListCollections)
-	protected.GET("/collections/:id", h.GetCollection)
-	protected.POST("/collections", h.CreateCollection)
+		protected.GET("/collections/:id/folders", h.ListFolders)
+		protected.POST("/collections/:id/folders", h.CreateFolder)
+		protected.PATCH("/collections/:id/folders/:folderId", h.UpdateFolder)
+		protected.DELETE("/collections/:id/folders/:folderId", h.DeleteFolder)
 
-	protected.GET("/collections/:id/folders", h.ListFolders)
-	protected.POST("/collections/:id/folders", h.CreateFolder)
-	protected.PATCH("/collections/:id/folders/:folderId", h.UpdateFolder)
-	protected.DELETE("/collections/:id/folders/:folderId", h.DeleteFolder)
-
-	protected.GET("/collections/:id/notes", h.ListNotes)
-	protected.POST("/collections/:id/notes", h.CreateNote)
-	protected.GET("/note/:id", h.GetNote)
-	protected.PATCH("/note/:id", h.UpdateNote)
-	protected.DELETE("/note/:id", h.DeleteNote)
-	protected.GET("/note/:id/comments", h.ListNoteComments)
-	protected.POST("/note/:id/comments", h.CreateNoteComment)
-	protected.PATCH("/comments/:id", h.UpdateNoteComment)
-	protected.DELETE("/comments/:id", h.DeleteNoteComment)
+		protected.GET("/collections/:id/notes", h.ListNotes)
+		protected.POST("/collections/:id/notes", h.CreateNote)
+		protected.GET("/note/:id", h.GetNote)
+		protected.PATCH("/note/:id", h.UpdateNote)
+		protected.DELETE("/note/:id", h.DeleteNote)
+		protected.GET("/note/:id/comments", h.ListNoteComments)
+		protected.POST("/note/:id/comments", h.CreateNoteComment)
+		protected.PATCH("/comments/:id", h.UpdateNoteComment)
+		protected.DELETE("/comments/:id", h.DeleteNoteComment)
+	}
 }
